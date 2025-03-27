ca09ba411693b8aa39a894c1b84a408daa01eccf
vintiyannick <yannickvinti@gmail.com>
2025-03-27 15:38:40 +0100

    var durch let ersetzt

--- main.js
+++ main.js
@@ -2,17 +2,17 @@
 Skript für die Neuseelandreise
 */
 
-//Karte initialisieren
-var map = L.map('map').setView([-34.423889, 172.6775], 13);
+// Karte initialisieren
+let map = L.map('map').setView([-34.423889, 172.6775], 13);
 
-//Hintergrund definieren
+// Hintergrund definieren
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-//Marker zeichnen
-var marker = L.marker([-34.423889, 172.6775]).addTo(map);
+// Marker zeichnen
+let marker = L.marker([-34.423889, 172.6775]).addTo(map);
 
-//Popup definieren und öffnen
+// Popup definieren und öffnen
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
